5199276f98d52a3cb2803486e584774b30cca663
gbgian <gianmorales98@icloud.com>
2021-02-16 04:10:50 +0000

    first commit

---
+++ count.java
@@ -0,0 +1,11 @@
+class count {
+	public static void main(String[] args){
+		int [] list = {1,2,3,4,5,6,7,8,9,10};
+		int sum = 0;
+
+for (int digt : list){
+	sum= sum+digt;
+}
+	System.out.println(sum);
+	}
+}
